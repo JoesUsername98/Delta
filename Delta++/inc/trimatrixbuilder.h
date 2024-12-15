7ea886bcd402f08c9aaee37c3f348605a63a46c6
Joe Osborne <joe.osborne1337@gmail.com>
2024-12-15 15:32:59 +0000

    add back ignored files (accident)

---
+++ Delta++/inc/trimatrixbuilder.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "triangularmatrix.h"
+
+namespace DPP
+{
+	class TriMatrixBuilder
+	{
+	public:
+		static TriMatrixBuilder create(const size_t steps, const double timeStep = 1.)
+		{
+			return TriMatrixBuilder(steps, timeStep);
+		}
+
+		TriMatrixBuilder& withUnderlyingValueAndVolatility(const double initialPrice, const double vol);
+		TriMatrixBuilder& withUnderlyingValueAndUpFactor(const double initialPrice, const double upFactor);
+		TriMatrixBuilder& withInterestRate(const double constantInterestRate);
+		TriMatrixBuilder& withPayoff(const OptionPayoffType optionType, const double strikePrice);
+		TriMatrixBuilder& withRiskNuetralProb();
+		TriMatrixBuilder& withPremium(const OptionExerciseType exerciseType);
+		TriMatrixBuilder& withDelta();
+		TriMatrixBuilder& withPsuedoOptimalStoppingTime();
+		TriMatrix build();
+		const std::string& getErrorMsg() const;
+	public:
+		bool m_hasError;
+
+	private:
+		explicit TriMatrixBuilder(const size_t steps, const double timeStep = 1.);
+
+	private:
+		TriMatrix m_result;
+		size_t m_timeSteps;
+		double m_timeStep;
+		double m_upFactor;
+		double m_downFactor;
+		double m_interestRate;
+		OptionExerciseType m_exerciseType;
+		std::string m_errorMsg;
+	};
+}
