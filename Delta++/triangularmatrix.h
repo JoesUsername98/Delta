771975d940e59268b9e276d26f95b843c7410ef1
Joe Osborne <joe.osborne1337@gmail.com>
2024-12-15 01:19:30 +0000

    temp commit

--- Delta++/triangularmatrix.h
+++ Delta++/triangularmatrix.h
@@ -11,7 +11,7 @@
 
 namespace DPP
 {
-	extern DPP_API struct ExpectableState
+	struct ExpectableState
 	{
 		double m_underlyingValue;
 		double m_payoff;
@@ -21,7 +21,7 @@ namespace DPP
 
 	};
 	
-	extern DPP_API struct State : ExpectableState
+	struct State : ExpectableState
 	{
 		double m_payoff;
 		double m_optionValue;
@@ -30,38 +30,38 @@ namespace DPP
 		std::optional<size_t> m_optimalExerciseTime;
 		ExpectableState m_expected;
 
-		DPP_API double getProbabilityTails() const;
+		double getProbabilityTails() const;
 	};
 
-	extern DPP_API struct Node
+	struct Node
 	{
 		State m_data;
 		size_t m_timeStep;
 		size_t m_downMoves;
 	};
 
-	extern DPP_API enum class OptionPayoffType
+	enum class OptionPayoffType
 	{
 		Call,
 		Put,
 		//TODO add more
 	};
 
-	extern DPP_API enum class OptionExerciseType
+	enum class OptionExerciseType
 	{
 		European,
 		American,
 		//TODO add more
 	};
 
-	extern DPP_API class TriMatrix
+	class TriMatrix
 	{
 	public:
 		friend class TriMatrixBuilder;
 
-		DPP_API const std::vector< std::vector< Node > >& getMatrix() const;
-		DPP_API double getDt() const;
-		DPP_API size_t getSteps() const;
+		const std::vector< std::vector< Node > >& getMatrix() const;
+		double getDt() const;
+		size_t getSteps() const;
 
 	private:
 		const double m_dt;
