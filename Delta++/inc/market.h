f99265bafcc2399558d3d1be0dc3418147b161e0
JoesUsername98 <joe.osborne1337@gmail.com>
2025-03-29 16:55:00 +0000

    Use const refs in engine factory

--- Delta++/inc/market.h
+++ Delta++/inc/market.h
@@ -12,21 +12,21 @@ namespace DPP
         m_vol( vol ), m_underlyingPrice( underlyingPrice ), m_interestRate( interestRate )
         {}
 
-        MarketData bumpVol ( double bump )
+        MarketData bumpVol ( double bump ) const 
         {
             MarketData bumpee = *this;
             bumpee.m_vol += bump;
             return bumpee;
         }
 
-        MarketData bumpUnderlying ( double bump )
+        MarketData bumpUnderlying ( double bump ) const
         {
             MarketData bumpee = *this;
             bumpee.m_underlyingPrice *= bump;
             return bumpee;
         }
         
-        MarketData bumpInterestRate ( double bump )
+        MarketData bumpInterestRate ( double bump ) const
         {
             MarketData bumpee = *this;
             bumpee.m_interestRate += bump;
