6dfd783562c6ff07fa184cba90c0eaaafaaa55f2
JoesUsername98 <joe.osborne1337@gmail.com>
2025-10-05 18:04:09 +0100

    Use box muller to get an constient distribution between os

--- Delta++Math/inc/distributions.h
+++ Delta++Math/inc/distributions.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <cmath>
+#include <random>
 
 namespace DPPMath
 {
     double cumDensity( double z );
     double probDensity( double z );
+    double box_muller( std::mt19937_64& rng );
 }
