af35c03a6d48588daae39f05961c603b336e237c
JoesUsername98 <joe.osborne1337@gmail.com>
2025-11-22 22:18:18 +0000

    Some clean up of large calcPV function

--- Delta++/inc/Delta++/monte_carlo_engine.h
+++ Delta++/inc/Delta++/monte_carlo_engine.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <vector>
+#include <ranges>
+#include <numeric>
+#include <algorithm>
+#include <cmath>
+#include <limits>
+
 #include "abstract_engine.h"
 
 namespace DPP
@@ -24,5 +31,66 @@ namespace DPP
 
     private:
 		void updatePrice( double& S, double dW, double dt ) const;
+        std::vector<double> simPaths(const CalcData& calc, const double dt ) const;
+        
+        template<typename PayoffFn>
+        double calcEuropeanPV(const CalcData& calc, const std::vector<double>& sims, double dt, PayoffFn payoff_fn) const;
+
+        template<typename PayoffFn>
+        double calcAmericanPV(const CalcData& calc, const std::vector<double>& sims, double dt, PayoffFn payoff_fn) const;
     };
+}
+
+namespace DPP 
+{
+    template<typename PayoffFn>
+    double MonteCarloEngine::calcEuropeanPV(const CalcData& calc, const std::vector<double>& sims, double dt, PayoffFn payoff_fn) const
+    {
+        const auto maturity_prices =
+            sims
+            | std::views::drop(calc.m_steps - 1)
+            | std::views::stride(calc.m_steps);
+
+        double sum = std::ranges::fold_left(
+            maturity_prices
+            | std::views::transform(payoff_fn)
+            , 0.0, std::plus{});
+
+        const double mean_payoff = sum / std::ranges::distance(maturity_prices);
+        return std::exp(-m_mkt.m_interestRate * m_trd.m_maturity) * mean_payoff;
+    }
+
+    template<typename PayoffFn>
+    double MonteCarloEngine::calcAmericanPV(const CalcData& calc, const std::vector<double>& sims, double dt, PayoffFn payoff_fn) const
+    {
+        const double mult = std::exp(-m_mkt.m_interestRate * dt);
+        std::vector<double> dfs;
+        dfs.reserve(calc.m_steps);
+        dfs.push_back(1.0);
+        for (size_t s = 1; s < calc.m_steps; ++s) { dfs.push_back(dfs[s - 1] * mult); }
+
+        auto final_pvs_view =
+            std::views::iota(size_t{0}, calc.m_sims)
+            | std::views::transform([&](size_t sim_idx) {
+                auto S_t0_sim = sims.begin() + sim_idx * calc.m_steps;
+
+                auto discounted_payoff =
+                    std::views::iota(size_t{0}, calc.m_steps)
+                    | std::views::transform([&](size_t step) {
+                        double S_t_sim = *(S_t0_sim + step);
+                        return payoff_fn(S_t_sim) * dfs[step];
+                    });
+
+                double sim_max = std::ranges::fold_left(
+                    discounted_payoff,
+                    std::numeric_limits<double>::lowest(),
+                    [](double a, double b) { return std::max(a, b); }
+                );
+
+                return sim_max;
+            });
+
+        const double sum_path_pvs = std::ranges::fold_left(final_pvs_view, 0.0, std::plus{});
+        return sum_path_pvs / calc.m_sims;
+    }
 }
