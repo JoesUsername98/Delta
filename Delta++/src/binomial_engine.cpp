2fbcfcdfad56d657dded9a309f15965edb2df460
JoesUsername98 <joe.osborne1337@gmail.com>
2025-11-29 22:27:01 +0000

    fix variable names

--- Delta++/src/binomial_engine.cpp
+++ Delta++/src/binomial_engine.cpp
@@ -5,7 +5,7 @@ namespace DPP
 {
     void BinomialEngine::calcPV( const CalcData& calc )
     {
-        TriMatrixBuilder buildResult = 
+        TriMatrixBuilder build_result = 
         TriMatrixBuilder::create( calc.m_steps, m_trd.m_maturity / calc.m_steps )
         .withUnderlyingValueAndVolatility( m_mkt.m_underlyingPrice, m_mkt.m_vol )
         .withInterestRate( m_mkt.m_interestRate )
@@ -13,141 +13,141 @@ namespace DPP
         .withRiskNuetralProb()
         .withPremium( m_trd.m_optionExerciseType );
         
-        if( buildResult.m_hasError )
-            m_errors.emplace( calc.m_calc , buildResult.getErrorMsg() );
+        if( build_result.m_hasError )
+            m_errors.emplace( calc.m_calc , build_result.getErrorMsg() );
         else
-            m_results.emplace( calc.m_calc , buildResult.build().getMatrix()[ 0 ].m_data.m_optionValue );
+            m_results.emplace( calc.m_calc , build_result.build().getMatrix()[ 0 ].m_data.m_optionValue );
     }
 
     void BinomialEngine::calcDelta( const CalcData& calc )
     {
-        CalcData pvOnly = calc;
-        pvOnly.m_calc = Calculation::PV;
+        CalcData pv_only = calc;
+        pv_only.m_calc = Calculation::PV;
 
-        MarketData bumpUp = m_mkt.bumpUnderlying( 1.005 );
-        BinomialEngine upCalc ( bumpUp, m_trd, pvOnly );
-        upCalc.run();
+        MarketData bump_up = m_mkt.bumpUnderlying( 1.005 );
+        BinomialEngine up_calc ( bump_up, m_trd, pv_only );
+        up_calc.run();
 
-        if( !upCalc.m_errors.empty() ) 
+        if( !up_calc.m_errors.empty() ) 
         {
-            for( const auto& [ upCalc , err ] : upCalc.m_errors )
+            for( const auto& [ key , err ] : up_calc.m_errors )
                 m_errors[ calc.m_calc ] += " "s + err;
             
             return;
         }
 
-        MarketData bumpDown = m_mkt.bumpUnderlying( .995 );
-        BinomialEngine downCalc ( bumpDown, m_trd, pvOnly );
-        downCalc.run();
-        if( !downCalc.m_errors.empty() ) 
+        MarketData bump_down = m_mkt.bumpUnderlying( .995 );
+        BinomialEngine down_calc ( bump_down, m_trd, pv_only );
+        down_calc.run();
+        if( !down_calc.m_errors.empty() ) 
         {
-            for( const auto& [ downCalc , err ] : downCalc.m_errors )
+            for( const auto& [ key , err ] : down_calc.m_errors )
                 m_errors[ calc.m_calc ] += " "s + err;
             
             return;
         }
 
-        const double pvUp = upCalc.m_results.at( Calculation::PV );
-        const double pvDown = downCalc.m_results.at( Calculation::PV );
-        m_results.emplace( calc.m_calc, pvUp - pvDown );
+        const double pv_up = up_calc.m_results.at( Calculation::PV );
+        const double pv_down = down_calc.m_results.at( Calculation::PV );
+        m_results.emplace( calc.m_calc, pv_up - pv_down );
     }
 
     void BinomialEngine::calcRho( const CalcData& calc )
     {
-        CalcData pvOnly = calc;
-        pvOnly.m_calc = Calculation::PV;
+        CalcData pv_only = calc;
+        pv_only.m_calc = Calculation::PV;
 
-        MarketData bumpUp = m_mkt.bumpInterestRate( 0.005 );
-        BinomialEngine upCalc ( bumpUp, m_trd, pvOnly );
-        upCalc.run();
+        MarketData bump_up = m_mkt.bumpInterestRate( 0.005 );
+        BinomialEngine up_calc ( bump_up, m_trd, pv_only );
+        up_calc.run();
 
-        if( !upCalc.m_errors.empty() ) 
+        if( !up_calc.m_errors.empty() ) 
         {
-            for( const auto& [ upCalc , err ] : upCalc.m_errors )
+            for( const auto& [ key , err ] : up_calc.m_errors )
                 m_errors[ calc.m_calc ] += " "s + err;
             
             return;
         }
 
-        MarketData bumpDown = m_mkt.bumpInterestRate (-0.005);
-        BinomialEngine downCalc ( bumpDown, m_trd, pvOnly );
-        downCalc.run();
-        if( !downCalc.m_errors.empty() ) 
+        MarketData bump_down = m_mkt.bumpInterestRate (-0.005);
+        BinomialEngine down_calc ( bump_down, m_trd, pv_only );
+        down_calc.run();
+        if( !down_calc.m_errors.empty() ) 
         {
-            for( const auto& [ downCalc , err ] : downCalc.m_errors )
+            for( const auto& [ key , err ] : down_calc.m_errors )
                 m_errors[ calc.m_calc ] += " "s + err;
             
             return;
         }
 
-        const double pvUp = upCalc.m_results.at( Calculation::PV );
-        const double pvDown = downCalc.m_results.at( Calculation::PV );
-        m_results.emplace( calc.m_calc, 100. * ( pvUp - pvDown ) );
+        const double pv_up = up_calc.m_results.at( Calculation::PV );
+        const double pv_down = down_calc.m_results.at( Calculation::PV );
+        m_results.emplace( calc.m_calc, 100. * ( pv_up - pv_down ) );
     }
 
     void BinomialEngine::calcVega( const CalcData& calc )
     {
-        CalcData pvOnly = calc;
-        pvOnly.m_calc = Calculation::PV;
+        CalcData pv_only = calc;
+        pv_only.m_calc = Calculation::PV;
 
-        MarketData bumpUp = m_mkt.bumpVol( 0.005 );
-        BinomialEngine upCalc ( bumpUp, m_trd, pvOnly );
-        upCalc.run();
+        MarketData bump_up = m_mkt.bumpVol( 0.005 );
+        BinomialEngine up_calc ( bump_up, m_trd, pv_only );
+        up_calc.run();
 
-        if( !upCalc.m_errors.empty() ) 
+        if( !up_calc.m_errors.empty() ) 
         {
-            for( const auto& [ upCalc , err ] : upCalc.m_errors )
+            for( const auto& [ key , err ] : up_calc.m_errors )
                 m_errors[ calc.m_calc ] += " "s + err;
             
             return;
         }
 
-        MarketData bumpDown = m_mkt.bumpVol( -0.005 );
-        BinomialEngine downCalc ( bumpDown, m_trd, pvOnly );
-        downCalc.run();
-        if( !downCalc.m_errors.empty() ) 
+        MarketData bump_down = m_mkt.bumpVol( -0.005 );
+        BinomialEngine down_calc ( bump_down, m_trd, pv_only );
+        down_calc.run();
+        if( !down_calc.m_errors.empty() ) 
         {
-            for( const auto& [ downCalc , err ] : downCalc.m_errors )
+            for( const auto& [ key , err ] : down_calc.m_errors )
                 m_errors[ calc.m_calc ] += " "s + err;
             
             return;
         }
 
-        const double pvUp = upCalc.m_results.at( Calculation::PV );
-        const double pvDown = downCalc.m_results.at( Calculation::PV );
-        m_results.emplace( calc.m_calc, (pvUp - pvDown)*100 );
+        const double pv_up = up_calc.m_results.at( Calculation::PV );
+        const double pv_down = down_calc.m_results.at( Calculation::PV );
+        m_results.emplace( calc.m_calc, (pv_up - pv_down)*100 );
     }
 
     void BinomialEngine::calcGamma( const CalcData& calc )
     {
-        CalcData deltaOnly = calc;
-        deltaOnly.m_calc = Calculation::Delta;
+        CalcData delta_only = calc;
+        delta_only.m_calc = Calculation::Delta;
 
-        MarketData bumpUp = m_mkt.bumpUnderlying( 1.005 );
-        BinomialEngine upCalc ( bumpUp, m_trd, deltaOnly );
-        upCalc.run();
+        MarketData bump_up = m_mkt.bumpUnderlying( 1.005 );
+        BinomialEngine up_calc ( bump_up, m_trd, delta_only );
+        up_calc.run();
 
-        if( !upCalc.m_errors.empty() ) 
+        if( !up_calc.m_errors.empty() ) 
         {
-            for( const auto& [ upCalc , err ] : upCalc.m_errors )
+            for( const auto& [ key , err ] : up_calc.m_errors )
                 m_errors[ calc.m_calc ] += " "s + err;
             
             return;
         }
 
-        MarketData bumpDown = m_mkt.bumpUnderlying( .995 );
-        BinomialEngine downCalc ( bumpDown, m_trd, deltaOnly );
-        downCalc.run();
-        if( !downCalc.m_errors.empty() ) 
+        MarketData bump_down = m_mkt.bumpUnderlying( .995 );
+        BinomialEngine down_calc ( bump_down, m_trd, delta_only );
+        down_calc.run();
+        if( !down_calc.m_errors.empty() ) 
         {
-            for( const auto& [ downCalc , err ] : downCalc.m_errors )
+            for( const auto& [ key , err ] : down_calc.m_errors )
                 m_errors[ calc.m_calc ] += " "s + err;
             
             return;
         }
 
-        const double deltaUp = upCalc.m_results.at( Calculation::Delta );
-        const double deltaDown = downCalc.m_results.at( Calculation::Delta );
-        m_results.emplace( calc.m_calc, deltaUp - deltaDown );
+        const double delta_up = up_calc.m_results.at( Calculation::Delta );
+        const double delta_down = down_calc.m_results.at( Calculation::Delta );
+        m_results.emplace( calc.m_calc, delta_up - delta_down );
     }
 }
