7bd5fdd936f9bdcb4189772b744ac0a6563a5886
JoesUsername98 <joe.osborne1337@gmail.com>
2025-11-30 14:40:01 +0000

    Move strategies to seperate files

--- Delta++/src/monte_carlo_engine.cpp
+++ Delta++/src/monte_carlo_engine.cpp
@@ -1,10 +1,8 @@
 #include <random>
 #include <future>
-#include <thread>
-#include <cmath>
 #include <stdexcept>
+#include <algorithm>
 
-#include <Delta++Math/distributions.h>
 #include "Delta++/monte_carlo_engine.h"
 
 
@@ -65,106 +63,14 @@ namespace DPP
         }
     }
 
-    double MCEuropeanExercise::price(const MonteCarloEngine& engine, const CalcData& calc,
-                                   const std::vector<double>& sims, double dt, const IMCPayoff& payoff) const
-    {
-        const auto maturity_prices =
-            sims
-            | std::views::drop(calc.m_steps - 1)
-            | std::views::stride(calc.m_steps);
-
-        double sum = std::ranges::fold_left(
-            maturity_prices
-            | std::views::transform([&](double s) { return payoff(s); })
-            , 0.0, std::plus<>());
-
-        const double mean_payoff = sum / std::ranges::distance(maturity_prices);
-        return std::exp(-engine.m_mkt.m_interestRate * engine.m_trd.m_maturity) * mean_payoff;
-    }
-
-    double MCAmericanExercise::price(const MonteCarloEngine& engine, const CalcData& calc,
-                                   const std::vector<double>& sims, double dt, const IMCPayoff& payoff) const
-    {
-        const double mult = std::exp(-engine.m_mkt.m_interestRate * dt);
-        std::vector<double> dfs;
-        dfs.reserve(calc.m_steps);
-        dfs.push_back(1.0);
-        for (size_t s = 1; s < calc.m_steps; ++s) { dfs.push_back(dfs[s - 1] * mult); }
-
-        auto final_pvs_view =
-            std::views::iota(size_t{0}, calc.m_sims)
-            | std::views::transform( [&](size_t sim_idx) {
-                auto S_t0_sim = sims.begin() + sim_idx * calc.m_steps;
-
-                auto discounted_payoff =
-                    std::views::iota(size_t{0}, calc.m_steps)
-                    | std::views::transform([&](size_t step) {
-                        double S_t_sim = *(S_t0_sim + step);
-                        return payoff(S_t_sim) * dfs[step];
-                    });
-
-                double sim_max = std::ranges::fold_left(
-                    discounted_payoff,
-                    std::numeric_limits<double>::lowest(),
-                    [](double a, double b) { return (std::max)(a, b); }
-                );
-
-                return sim_max;
-            });
-
-        const double sum_path_pvs = std::ranges::fold_left(final_pvs_view, 0.0, std::plus<>());
-        return sum_path_pvs / calc.m_sims;
-    }
-
-    std::vector<double> MonteCarloEngine::simPaths(const CalcData& calc, const double dt) const 
-    {
-        const auto sqrt_dt = std::sqrt(dt);
-        std::vector<double> sims(calc.m_sims * calc.m_steps, m_mkt.m_underlyingPrice);
-
-        auto worker = [&](size_t start_sim, size_t end_sim, size_t thread_id) {
-            static thread_local std::seed_seq seq{ 42 + thread_id };
-            static thread_local std::mt19937_64 rng{ seq };
-            static thread_local std::uniform_real_distribution<double> unif(0.0, 1.0);
-
-            for (size_t sim_idx = start_sim; sim_idx < end_sim; ++sim_idx)
-            {
-                double s = m_mkt.m_underlyingPrice;
-                for (size_t step_index = 1; step_index < calc.m_steps; ++step_index)
-                {
-                    const auto idx = sim_idx * calc.m_steps + step_index;
-                    const double u = unif(rng);
-                    const double z = DPPMath::invCumDensity(u);
-                    const double dW = sqrt_dt * z;
-                    m_scheme->updatePrice(s, dW, dt, m_mkt);
-                    sims[idx] = s;
-                }
-            }
-            };
-
-        const size_t n_threads = std::thread::hardware_concurrency();
-        const size_t sims_per_thread = calc.m_sims / n_threads;
-        std::vector<std::thread> threads;
-        threads.reserve(n_threads);
-        size_t start = 0;
-        for (size_t t = 0; t < n_threads; ++t)
-        {
-            size_t end = (t == n_threads - 1) ? calc.m_sims : start + sims_per_thread;
-            threads.emplace_back(worker, start, end, t);
-            start = std::min(end, calc.m_sims);
-        }
-        for (auto& t : threads) { t.join(); };
-
-        return sims;
-    }
-
     void MonteCarloEngine::calcPV( const CalcData& calc )
     {
         const auto dt =  m_trd.m_maturity / static_cast<double>( calc.m_steps );
-        std::vector<double> sims = simPaths( calc, dt );
+		std::vector<double> sims = m_scheme->simPaths(m_mkt, calc, dt);
 
         try 
         {
-            const double pv = m_exercise->price(*this, calc, sims, dt, *m_payoff);
+            const double pv = m_exercise->price(m_trd, m_mkt, calc, sims, dt, *m_payoff);
             m_results.emplace(calc.m_calc, pv);
         }
         catch (const std::exception& e)
