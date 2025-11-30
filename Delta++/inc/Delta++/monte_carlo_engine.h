1192013bc22c24ba491ba240e260bab5a882cacd
JoesUsername98 <joe.osborne1337@gmail.com>
2025-11-30 13:44:14 +0000

    Use strategy pattern for MC payoff, exercise and path gen

--- Delta++/inc/Delta++/monte_carlo_engine.h
+++ Delta++/inc/Delta++/monte_carlo_engine.h
@@ -6,20 +6,92 @@
 #include <algorithm>
 #include <cmath>
 #include <limits>
+#include <memory>
 
 #include "abstract_engine.h"
 
 namespace DPP
 {
+    // Forward declaration
+    class MonteCarloEngine;
+
+    // Path generation strategy interface
+    struct IPathScheme
+    {
+        virtual void updatePrice(double& S, double dW, double dt, const MarketData& mkt) const = 0;
+        virtual ~IPathScheme() = default;
+    };
+
+    struct ExactScheme : IPathScheme
+    {
+        void updatePrice(double& S, double dW, double dt, const MarketData& mkt) const override
+        {
+            S = S * std::exp((mkt.m_interestRate - 0.5 * mkt.m_vol * mkt.m_vol) * dt + mkt.m_vol * dW);
+        }
+    };
+
+    struct EulerScheme : IPathScheme
+    {
+        void updatePrice(double& S, double dW, double dt, const MarketData& mkt) const override
+        {
+            S = S * (1 + mkt.m_interestRate * dt + mkt.m_vol * dW);
+        }
+    };
+
+    struct MilsteinScheme : IPathScheme
+    {
+        void updatePrice(double& S, double dW, double dt, const MarketData& mkt) const override
+        {
+            S = S * (1 + mkt.m_interestRate * dt + mkt.m_vol * dW + 0.5 * mkt.m_vol * mkt.m_vol * (dW * dW - dt));
+        }
+    };
+
+    struct IMCPayoff
+    {
+        virtual double operator()(double s) const = 0;
+        virtual ~IMCPayoff() = default;
+    };
+
+    struct MCCallPayoff : IMCPayoff
+    {
+        explicit MCCallPayoff(double strike) : m_strike(strike) {}
+        double operator()(double s) const override { return std::max<double>(s - m_strike, 0.0); }
+    private:
+        double m_strike;
+    };
+
+    struct MCPutPayoff : IMCPayoff
+    {
+        explicit MCPutPayoff(double strike) : m_strike(strike) {}
+        double operator()(double s) const override { return std::max<double>(m_strike - s, 0.0); }
+    private:
+        double m_strike;
+    };
+
+    struct IMCExercise
+    {
+        virtual double price(const MonteCarloEngine& engine, const CalcData& calc,
+                             const std::vector<double>& sims, double dt, const IMCPayoff& payoff) const = 0;
+        virtual ~IMCExercise() = default;
+    };
+
+    struct MCEuropeanExercise : IMCExercise
+    {
+        double price(const MonteCarloEngine& engine, const CalcData& calc,
+                     const std::vector<double>& sims, double dt, const IMCPayoff& payoff) const override;
+    };
+
+    struct MCAmericanExercise : IMCExercise
+    {
+        double price(const MonteCarloEngine& engine, const CalcData& calc,
+                     const std::vector<double>& sims, double dt, const IMCPayoff& payoff) const override;
+    };
+
     class MonteCarloEngine : public AbstractEngine
     {
     public:
-        MonteCarloEngine( const MarketData& mkt, const TradeData& trd, const CalcData& calc ) :
-        AbstractEngine( mkt, trd, calc )
-        {}
-        MonteCarloEngine( const MarketData& mkt, const TradeData& trd, const std::vector<CalcData>& calc ) :
-        AbstractEngine( mkt, trd, calc )
-        {}
+        MonteCarloEngine( const MarketData& mkt, const TradeData& trd, const CalcData& calc );
+        MonteCarloEngine( const MarketData& mkt, const TradeData& trd, const std::vector<CalcData>& calc );
         virtual ~MonteCarloEngine() = default;
 
     protected:
@@ -30,67 +102,13 @@ namespace DPP
         void calcGamma( const CalcData& calc ) override;  
 
     private:
-		void updatePrice( double& S, double dW, double dt ) const;
+        void initStrategies();
         std::vector<double> simPaths(const CalcData& calc, const double dt ) const;
-        
-        template<typename PayoffFn>
-        double calcEuropeanPV(const CalcData& calc, const std::vector<double>& sims, double dt, PayoffFn payoff_fn) const;
 
-        template<typename PayoffFn>
-        double calcAmericanPV(const CalcData& calc, const std::vector<double>& sims, double dt, PayoffFn payoff_fn) const;
+        const PathSchemeType m_pathSchemeType;
+
+        std::unique_ptr<IMCPayoff> m_payoff;
+        std::unique_ptr<IMCExercise> m_exercise;
+        std::unique_ptr<IPathScheme> m_scheme;
     };
-}
-
-namespace DPP 
-{
-    template<typename PayoffFn>
-    double MonteCarloEngine::calcEuropeanPV(const CalcData& calc, const std::vector<double>& sims, double dt, PayoffFn payoff_fn) const
-    {
-        const auto maturity_prices =
-            sims
-            | std::views::drop(calc.m_steps - 1)
-            | std::views::stride(calc.m_steps);
-
-        double sum = std::ranges::fold_left(
-            maturity_prices
-            | std::views::transform(payoff_fn)
-            , 0.0, std::plus{});
-
-        const double mean_payoff = sum / std::ranges::distance(maturity_prices);
-        return std::exp(-m_mkt.m_interestRate * m_trd.m_maturity) * mean_payoff;
-    }
-
-    template<typename PayoffFn>
-    double MonteCarloEngine::calcAmericanPV(const CalcData& calc, const std::vector<double>& sims, double dt, PayoffFn payoff_fn) const
-    {
-        const double mult = std::exp(-m_mkt.m_interestRate * dt);
-        std::vector<double> dfs;
-        dfs.reserve(calc.m_steps);
-        dfs.push_back(1.0);
-        for (size_t s = 1; s < calc.m_steps; ++s) { dfs.push_back(dfs[s - 1] * mult); }
-
-        auto final_pvs_view =
-            std::views::iota(size_t{0}, calc.m_sims)
-            | std::views::transform( [&](size_t sim_idx) {
-                auto S_t0_sim = sims.begin() + sim_idx * calc.m_steps;
-
-                auto discounted_payoff =
-                    std::views::iota(size_t{0}, calc.m_steps)
-                    | std::views::transform([&](size_t step) {
-                        double S_t_sim = *(S_t0_sim + step);
-                        return payoff_fn(S_t_sim) * dfs[step];
-                    });
-
-                double sim_max = std::ranges::fold_left(
-                    discounted_payoff,
-                    std::numeric_limits<double>::lowest(),
-                    [](double a, double b) { return (std::max)(a, b); }
-                );
-
-                return sim_max;
-            });
-
-        const double sum_path_pvs = std::ranges::fold_left(final_pvs_view, 0.0, std::plus{});
-        return sum_path_pvs / calc.m_sims;
-    }
 }
