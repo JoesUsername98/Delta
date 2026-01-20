4655863f059795e4c6394d4ab678dc2509929136
JoesUsername98 <joe.osborne1337@gmail.com>
2026-01-20 00:35:00 +0000

    fix build issues

--- walnut_custom/ApplicationOpenGL.cpp
+++ walnut_custom/ApplicationOpenGL.cpp
@@ -1,23 +1,19 @@
 #include "ApplicationOpenGL.h"
-
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
-#include <stdio.h>          
-#include <stdlib.h>         
-#define GLFW_INCLUDE_OPENGL
+// Use system OpenGL headers directly on Windows
+#ifdef _WIN32
+#include <windows.h>
+#include <GL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <iostream>
-
-// Embedded font
-#include "Walnut/ImGui/Roboto-Regular.embed"
+#include <algorithm>
 
 extern bool g_ApplicationRunning;
-
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
-#endif
 
 namespace Walnut {
 
@@ -27,15 +23,11 @@ namespace Walnut {
 		: m_Specification(specification)
 	{
 		s_Instance = this;
-
-		Init();
 	}
 
 	Application::~Application()
 	{
 		Shutdown();
-
-		s_Instance = nullptr;
 	}
 
 	Application& Application::Get()
@@ -43,78 +35,86 @@ namespace Walnut {
 		return *s_Instance;
 	}
 
-	void Application::Init()
+	void Application::Run()
 	{
-		// Setup GLFW
-		if (!glfwInit())
+		Init();
+
+		while (!glfwWindowShouldClose(m_WindowHandle))
 		{
-			fprintf(stderr, "Failed to initialize GLFW\n");
-			exit(1);
+			// Poll events
+			glfwPollEvents();
+
+			// Update layers
+			for (auto& layer : m_LayerStack)
+				layer->OnUpdate(0.0f); // Simple fixed timestep for now
+
+			// Render
+			int display_w, display_h;
+			glfwGetFramebufferSize(m_WindowHandle, &display_w, &display_h);
+			glViewport(0, 0, display_w, display_h);
+			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			// Start the ImGui frame
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+
+			// Render UI layers
+			for (auto& layer : m_LayerStack)
+				layer->OnUIRender();
+
+			// Render ImGui
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+			glfwSwapBuffers(m_WindowHandle);
 		}
 
-		// Decide GL+GLSL versions
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-		// GL ES 2.0 + GLSL 100
-		const char* glsl_version = "#version 100";
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-		glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-#elif defined(__APPLE__)
-		// GL 3.2 + GLSL 150
-		const char* glsl_version = "#version 150";
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
-#else
-		// GL 3.0 + GLSL 130
-		const char* glsl_version = "#version 130";
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-		//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-		//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-#endif
+		g_ApplicationRunning = false;
+	}
 
-		// Create window with graphics context
+	void Application::Init()
+	{
+		// Initialize GLFW
+		if (!glfwInit())
+		{
+			std::cerr << "Failed to initialize GLFW" << std::endl;
+			return;
+		}
+
+		// GL 3.3 + GLSL 330
+		const char* glsl_version = "#version 330";
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+		// Create window
 		m_WindowHandle = glfwCreateWindow(m_Specification.Width, m_Specification.Height, m_Specification.Name.c_str(), NULL, NULL);
 		if (m_WindowHandle == NULL)
 		{
-			fprintf(stderr, "Failed to create GLFW window\n");
+			std::cerr << "Failed to create GLFW window" << std::endl;
 			glfwTerminate();
-			exit(1);
+			return;
 		}
-
 		glfwMakeContextCurrent(m_WindowHandle);
-		glfwSwapInterval(1); // Enable vsync
 
 		// Setup Dear ImGui context
 		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
 		ImGuiIO& io = ImGui::GetIO(); (void)io;
-		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
 		// Setup Dear ImGui style
 		ImGui::StyleColorsDark();
 
-		// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
-		ImGuiStyle& style = ImGui::GetStyle();
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-		{
-			style.WindowRounding = 0.0f;
-			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-		}
-
-		// Load default font
-		ImFontConfig fontConfig;
-		fontConfig.FontDataOwnedByAtlas = false;
-		ImFont* robotoFont = io.Fonts->AddFontFromMemoryTTF((void*)g_RobotoRegular, sizeof(g_RobotoRegular), 20.0f, &fontConfig);
-		io.FontDefault = robotoFont;
-
 		// Setup Platform/Renderer backends
 		ImGui_ImplGlfw_InitForOpenGL(m_WindowHandle, true);
 		ImGui_ImplOpenGL3_Init(glsl_version);
+
+		// Attach layers
+		for (auto& layer : m_LayerStack)
+			layer->OnAttach();
 	}
 
 	void Application::Shutdown()
@@ -131,105 +131,6 @@ namespace Walnut {
 
 		glfwDestroyWindow(m_WindowHandle);
 		glfwTerminate();
-
-		g_ApplicationRunning = false;
-	}
-
-	void Application::Run()
-	{
-		m_Running = true;
-
-		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-		ImGuiIO& io = ImGui::GetIO();
-
-		// Main loop
-		while (!glfwWindowShouldClose(m_WindowHandle) && m_Running)
-		{
-			// Poll and handle events (inputs, window resize, etc.)
-			glfwPollEvents();
-
-			// Start the Dear ImGui frame
-			ImGui_ImplOpenGL3_NewFrame();
-			ImGui_ImplGlfw_NewFrame();
-			ImGui::NewFrame();
-
-			static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
-
-			// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-			// because it would be confusing to have two docking targets within each others.
-			ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-			const ImGuiViewport* viewport = ImGui::GetMainViewport();
-			ImGui::SetNextWindowPos(viewport->WorkPos);
-			ImGui::SetNextWindowSize(viewport->WorkSize);
-			ImGui::SetNextWindowViewport(viewport->ID);
-			ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-			ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-			window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-			window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-
-			// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
-			// and handle the pass-thru hole, so we ask Begin() to not render a background.
-			if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-				window_flags |= ImGuiWindowFlags_NoBackground;
-
-			// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
-			// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
-			// all active windows docked into it will lose their parent and become undocked.
-			// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
-			// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
-			ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-			ImGui::Begin("DockSpace Demo", nullptr, window_flags);
-			ImGui::PopStyleVar();
-
-			ImGui::PopStyleVar(2);
-
-			// Submit the DockSpace
-			if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
-			{
-				ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-				ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-			}
-
-			if (ImGui::BeginMenuBar())
-			{
-				if (m_MenubarCallback)
-					m_MenubarCallback();
-
-				ImGui::EndMenuBar();
-			}
-
-			for (auto& layer : m_LayerStack)
-				layer->OnUIRender();
-
-			ImGui::End();
-
-			// Rendering
-			ImGui::Render();
-			int display_w, display_h;
-			glfwGetFramebufferSize(m_WindowHandle, &display_w, &display_h);
-			glViewport(0, 0, display_w, display_h);
-			glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-			glClear(GL_COLOR_BUFFER_BIT);
-			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-			// Update and Render additional Platform Windows
-			// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
-			//  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
-			if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-			{
-				GLFWwindow* backup_current_context = glfwGetCurrentContext();
-				ImGui::UpdatePlatformWindows();
-				ImGui::RenderPlatformWindowsDefault();
-				glfwMakeContextCurrent(backup_current_context);
-			}
-
-			glfwSwapBuffers(m_WindowHandle);
-
-			float time = GetTime();
-			m_FrameTime = time - m_LastFrameTime;
-			m_TimeStep = glm::min<float>(m_FrameTime, 0.0333f);
-			m_LastFrameTime = time;
-		}
 	}
 
 	void Application::Close()
