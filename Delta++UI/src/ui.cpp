8995cd2aaef06e18df3afa9730a49310787b74e6
JoesUsername98 <joe.osborne1337@gmail.com>
2026-03-31 16:42:56 +0100

    Open Local vol window on startup

--- Delta++UI/src/ui.cpp
+++ Delta++UI/src/ui.cpp
@@ -17,6 +17,7 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 	Walnut::Application* app = new Walnut::Application(spec);
 	app->PushLayer<PricerView>();
 	app->PushLayer<ApiTesterWindow>();
+	app->PushLayer<LocalVolSurfaceWindow>();
 	app->SetMenubarCallback([app]()
 	{
 		if (ImGui::BeginMenu("File"))
